80fa6cc4eceec1cfb4ba985c72064280f7506830
Preston Chandler <pchandler858@gmail.com>
2019-04-24 00:00:57 -0700

    ugly but working so far

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Tilt from "react-tilt";
+import brain from "./brain.png";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="ma3 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 250, width: 250 }}
+      >
+        <div className="Tilt-inner pa5">
+          <img
+            style={{ paddingTop: "5px", height: "126px" }}
+            src={brain}
+            alt="brain logo"
+          />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
