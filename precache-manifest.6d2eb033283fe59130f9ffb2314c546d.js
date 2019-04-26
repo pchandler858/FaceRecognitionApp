a477fff71e405b3976a7730a007937a68f68d7ff
Preston Chandler <pchandler858@gmail.com>
2019-04-25 23:18:43 -0700

    Updates

---
+++ precache-manifest.6d2eb033283fe59130f9ffb2314c546d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bda4d7a77816e15ec291dbe16554dfd",
+    "url": "/FaceRecognitionApp/index.html"
+  },
+  {
+    "revision": "5d2befed9c1c70fd303b",
+    "url": "/FaceRecognitionApp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d69bef5502f9b7a39ec2",
+    "url": "/FaceRecognitionApp/static/css/main.31fbcae7.chunk.css"
+  },
+  {
+    "revision": "5d2befed9c1c70fd303b",
+    "url": "/FaceRecognitionApp/static/js/2.31c73bdb.chunk.js"
+  },
+  {
+    "revision": "d69bef5502f9b7a39ec2",
+    "url": "/FaceRecognitionApp/static/js/main.257dcbb4.chunk.js"
+  },
+  {
+    "revision": "5068c2d7c5a4aac02c0a",
+    "url": "/FaceRecognitionApp/static/js/runtime~main.23fdf8f5.js"
+  }
+]);
