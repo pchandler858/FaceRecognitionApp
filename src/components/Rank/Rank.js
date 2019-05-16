d587a5e67a61ba040f513fe7fa2cf0c9495bd211
Preston Chandler <pchandler858@gmail.com>
2019-05-15 22:48:39 -0700

    finished up integrating the backend

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,7 +3,9 @@ import React from "react";
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className="white b f3">{`${name}, your current rank is...`}</div>
+      <div className="white b f3">
+        {`${name}, your current entry count is...`}
+      </div>
       <div className="white b f1">{entries}</div>
     </div>
   );
